38ed4425dce65f49119b7725c83a1d59014ed38a
kangho lee <ibcylon@naver.com>
2025-10-09 18:28:46 +0900

    Initial Commit

---
+++ GithubActionTest/GithubActionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionTestApp.swift
+//  GithubActionTest
+//
+//  Created by Kanghos on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
